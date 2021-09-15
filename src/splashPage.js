f962d241dd20c2fff683f24371febf87939f9637
iam33q <irisandrei97@gmail.com>
2021-09-15 20:35:19 +0100

    Spelling errors fixed

--- src/splashPage.js
+++ src/splashPage.js
@@ -12,10 +12,10 @@ class SplashPage extends React.Component {
                     this service is always improving and 
                     helping people reach their destination 
                     without any unnecessary hassle.</p><br/>
-                <p>PLease click below to register and 
+                <p>Please click below to register and 
                     enjoy our full services.</p><br/>
                 <button onClick={() => {return false} }>Register</button><br/>
-                <p>If you arealready a member, you may log in below:</p><br/>
+                <p>If you already a member, you may log in below:</p><br/>
                 <button onClick={() => {return false} }>Log In</button><br/>
             </div>
         )
