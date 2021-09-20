d07a0da397a5e5b4edd1929f6a1655b1177133f2
svtranter <82532670+svtranter@users.noreply.github.com>
2021-09-20 21:22:20 +0100

    Tidied up

--- src/Login.js
+++ src/Login.js
@@ -1,37 +1,46 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 function Login() {
-    return (
-        <Container className="Login" >
-            <div className="container-fluid h-100 d-flex justify-content-center align-items-center">
-                <Card>
-                    <Card.Title className="mt-3 mb-3 ms-1 me-1">Enter your email address and password below:</Card.Title>
-                    <Form>
-                        <Form.Group className="mb-3 ms-1 me-1" controlId="formBasicEmail">
-                            <Form.Label>Email Address:</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
+  return (
+    <Container className="Login">
+      <div className="container-fluid h-100 d-flex justify-content-center align-items-center">
+        <Card>
+          <Card.Title className="mt-3 mb-3 ms-1 me-1">
+            Enter your email address and password below:
+          </Card.Title>
+          <Form>
+            <Form.Group className="mb-3 ms-1 me-1" controlId="formBasicEmail">
+              <Form.Label>Email Address:</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
 
-                        <Form.Group className="mb-3 ms-1 me-1" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <div className="text-center">
-                            <Button variant="warning" type="submit" className="text-center mb-3">
-                                Login
-                            </Button>
-                        </div>
-                    </Form>
-                </Card>
+            <Form.Group
+              className="mb-3 ms-1 me-1"
+              controlId="formBasicPassword"
+            >
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <div className="text-center">
+              <Button
+                variant="warning"
+                type="submit"
+                className="text-center mb-3"
+              >
+                Login
+              </Button>
             </div>
-        </Container>
-    )
+          </Form>
+        </Card>
+      </div>
+    </Container>
+  );
 }
 
 export default Login;
