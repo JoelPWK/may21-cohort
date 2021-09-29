c9f81fdf188a76dd83dd66f25541d62e7e1ccc1d
svtranter <82532670+svtranter@users.noreply.github.com>
2021-09-29 19:42:16 +0100

    Added OnLogin

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Button from "react-bootstrap/Button";
@@ -6,41 +6,52 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 
-function Login() {
-  return (
-    <Container className="Login">
-      <div className="container-fluid h-100 d-flex justify-content-center align-items-center">
-        <Card>
-          <Card.Title className="mt-3 mb-3 ms-1 me-1">
-            Enter your email address and password below:
-          </Card.Title>
-          <Form>
-            <Form.Group className="mb-3 ms-1 me-1" controlId="formBasicEmail">
-              <Form.Label>Email Address:</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+export class Login extends Component {
 
-            <Form.Group
-              className="mb-3 ms-1 me-1"
-              controlId="formBasicPassword"
-            >
-              <Form.Label>Password:</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <div className="text-center">
-              <Button
-                variant="warning"
-                type="submit"
-                className="text-center mb-3"
+  OnLogin(event) {
+    //collect form inputs - email, password
+    var email = event.target.email.value;
+    var password = event.target.password.value;
+
+    //check details with database
+  }
+
+  render(){
+    return (
+      <Container className="Login">
+        <div className="container-fluid h-100 d-flex justify-content-center align-items-center">
+          <Card id="loginCard">
+            <Card.Title className="mt-3 mb-3 ms-1 me-1">
+              Enter your email address and password below:
+            </Card.Title>
+            <Form onSubmit={this.OnLogin}>
+              <Form.Group className="mb-3 ms-1 me-1" controlId="formBasicEmail">
+                <Form.Label>Email Address:</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" name="email" />
+              </Form.Group>
+
+              <Form.Group
+                className="mb-3 ms-1 me-1"
+                controlId="formBasicPassword"
               >
-                Login
-              </Button>
-            </div>
-          </Form>
-        </Card>
-      </div>
-    </Container>
-  );
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type="password" placeholder="Password" name="password" />
+              </Form.Group>
+              <div className="text-center">
+                <Button
+                  id="loginButton"
+                  type="submit"
+                  className="text-center mb-3"
+                >
+                  Login
+                </Button>
+              </div>
+            </Form>
+          </Card>
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default Login;
