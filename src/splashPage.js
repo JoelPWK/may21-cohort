c68a27c424bc37e73f58249315518aa54a433b13
svtranter <82532670+svtranter@users.noreply.github.com>
2021-11-20 11:58:45 +0000

    Added Bootstrap

--- src/splashPage.js
+++ src/splashPage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.scss";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Button from "react-bootstrap/Button";
 class SplashPage extends React.Component {
     constructor(props){
         super(props)
-
     this.state = {
         isHidden: true
     }
@@ -12,28 +13,24 @@ class SplashPage extends React.Component {
 //   closeButton(){
 //       return <button onClick={this.toggleHidden.bind(this)}>Hide/Show</button>
 //   }
-
     handleChange = (event) => {
         this.props.closeSplash(false); //Define custom broadcast event closeSplash
     }
-
     render(){
-        return(            
-        <div className={"main-text"}>
-            <h2>Welcome to StopStopGo!</h2>
-            <p>Through the input of users like you, 
-                this service is always improving and 
-                helping people reach their destination 
-                without any unnecessary hassle.</p>
-            <p>Please click below to register and 
-                enjoy our full services.</p>
-            <button onClick={() => {return false} }>Register</button><br/>
-            <p>If you already a member, you may log in below:</p>
-            <button onClick={() => {return false} }>Log In</button>
-            <button onClick={this.handleChange.bind(this)}>Hide/Show</button>
-        </div>
-        )
-    }
-}
-  
+        return(
+                <div class="containerSplash">
+                        <div class="container inner">
+                            <div class="inner cover">
+                                <h1 class="cover-heading">Stop Stop GO</h1>
+                                <p class="p1">Through the input of users like you,
+                                this service is always improving and
+                                without any unnecessary hassle.</p>
+                                <p class="p2">Click below to enter.</p>
+                                <div className="text-center">
+                                    <Button id="loginButton" type="submit" className="text-center mb-3" onClick={this.handleChange.bind(this)}>Continue</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+    )}}
   export default SplashPage;
